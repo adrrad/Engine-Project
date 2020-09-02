f12864d6fe6859574aee116b6c418144f1f61e88
Adrian Radziszewski <adra@itu.dk>
2020-09-02 18:09:15 +0200

    Added new definitions

---
+++ engine/include/gui/SceneInspector.hpp
@@ -0,0 +1,14 @@
+#pragma once
+
+namespace GUI
+{
+
+class SceneInspector
+{
+private:
+    
+public:
+
+}; 
+
+} // namespace GUI
---
+++ engine/src/gui/SceneInspector.cpp
@@ -0,0 +1,9 @@
+#include "gui/SceneInspector.hpp"
+#include "renderer/Scene.hpp"
+
+namespace GUI
+{
+
+
+
+} // namespace GUI
